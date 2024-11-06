b6998963f3e25ade4380c72670742b138bf0aa8d
Letícia <leticia.c.rodrigues0@gmail.com>
2024-11-06 16:11:45 -0300

    Pending tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,6 +12,10 @@ enum class TipoRoupa {
             }
         }
     }
+
+    fun equalsTo(str: String) : Boolean{
+        return str == this.name
+    }
 }
 
 enum class Tamanho {
@@ -26,6 +30,10 @@ enum class Tamanho {
             }
         }
     }
+
+    fun equalsTo(str: String) : Boolean{
+        return str == this.name
+    }
 }
 
 enum class TipoEletronico {
@@ -36,9 +44,18 @@ enum class TipoEletronico {
             return try {
                 valueOf(str)
             } catch (e: IllegalArgumentException) {
-                OUTROS
+                if(str == "VIDEO-GAME")
+                    VIDEO_GAME
+                else
+                    null
             }
         }
+    }
+
+    fun equalsTo(str: String) : Boolean{
+        val strModified = str.replace('-', '_')
+
+        return strModified == this.name
     }
 }
 
@@ -50,9 +67,13 @@ enum class TipoColecionavel {
             return try {
                 valueOf(str)
             } catch (e: IllegalArgumentException) {
-                OUTROS
+                null
             }
         }
+    }
+
+    fun equalsTo(str: String) : Boolean{
+        return str == this.name
     }
 }
 
@@ -64,9 +85,13 @@ enum class MaterialFabricacao {
             return try {
                 valueOf(str)
             } catch (e: IllegalArgumentException) {
-                OUTROS
+                null
             }
         }
+    }
+
+    fun equalsTo(str: String) : Boolean{
+        return str == this.name
     }
 }
 
@@ -82,9 +107,15 @@ enum class Relevancia {
             }
         }
     }
+
+    fun equalsTo(str: String) : Boolean{
+        return str == this.name
+    }
 }
 
-open class Produto(val nome: String, val preco_compra: Double, val preco_venda: Double, var cod: String)
+open abstract class Produto(val nome: String, val preco_compra: Double, val preco_venda: Double, var cod: String){
+    abstract fun atendeAosCriterios(info: InfoBusca): Boolean
+}
 
 class Roupa(
     cod: String,
@@ -126,6 +157,17 @@ class Roupa(
 
         return res
     }
+
+    override fun atendeAosCriterios(info: InfoBusca): Boolean {
+        var res = true
+        res = res && (info.categoria == "-" || info.categoria == "ROUPA")
+        res = res && (info.tipo == "-" || this.tipo?.equalsTo(info.tipo) ?: false)
+        res = res && (info.tamanho == "-" || this.tamanho?.equalsTo(info.tamanho) ?: false)
+        res = res && (info.cor_primaria == "-" || this.cor_primaria == info.cor_primaria)
+        res = res && (info.cor_secundaria == "-" || this.cor_secundaria == info.cor_secundaria)
+
+        return res
+    }
 }
 
 class Eletronico(
@@ -161,6 +203,16 @@ class Eletronico(
         res += "Tipo: ${this.tipo ?: "-"}, "
         res += "Versão: ${this.versao}, "
         res += "Ano: ${this.ano}"
+
+        return res
+    }
+
+    override fun atendeAosCriterios(info: InfoBusca): Boolean {
+        var res = true
+        res = res && (info.categoria == "-" || info.categoria == "ELETRONICO")
+        res = res && (info.tipo == "-" || this.tipo?.equalsTo(info.tipo) ?: false)
+        res = res && (info.versao == "-" || this.versao == info.versao)
+        res = res && (info.ano == "-" || this.ano == info.ano.toInt())
 
         return res
     }
@@ -214,10 +266,24 @@ class Colecionavel(
 
         return res
     }
+
+    override fun atendeAosCriterios(info: InfoBusca): Boolean {
+        var res = true
+        res = res && (info.categoria == "-" || info.categoria == "COLECIONAVEL")
+        res = res && (info.tipo == "-" || this.tipo?.equalsTo(info.tipo) ?: false)
+        res = res && (info.material == "-" || this.material?.equalsTo(info.material) ?: false)
+        res = res && (info.relevancia == "-" || this.relevancia?.equalsTo(info.relevancia) ?: false)
+        res = res && (info.tamanho == "-" || this.tamanho == info.tamanho.toDouble())
+
+        return res
+    }
 }
 
-fun leEntradas(path: String, produtos_mp: MutableMap<String, Produto>, estoque_mp: MutableMap<String, Int>) {
-    var comprasFilePath = path
+data class InfoEstoque(var prod: Produto, var qtd: Int)
+data class InfoBalancete(var compras: Double, var vendas: Double, var balancete: Double)
+
+fun leCompras(path: String, estoque_mp: MutableMap<String, InfoEstoque>, balancete_dt: InfoBalancete) {
+    var comprasFilePath = path + "/compras.csv"
 
     File(comprasFilePath).bufferedReader().useLines { lines ->
         for (line in lines.drop(1)) {
@@ -261,42 +327,168 @@ fun leEntradas(path: String, produtos_mp: MutableMap<String, Produto>, estoque_m
             }
 
             p?.let {
-                if (!produtos_mp.containsKey(p.cod)) {
-                    produtos_mp[p.cod] = p
-                    estoque_mp[p.cod] = qtd
+                if (!estoque_mp.containsKey(p.cod)) {
+                    estoque_mp[p.cod] = InfoEstoque(p, qtd)
                 } else {
-                    estoque_mp[p.cod] = qtd + estoque_mp[p.cod]!!
+                    estoque_mp[p.cod]!!.qtd = qtd + estoque_mp[p.cod]!!.qtd
                 }
+
+                balancete_dt.compras += qtd * p.preco_compra
+                balancete_dt.balancete -= qtd * p.preco_compra
             }
         }
     }
 }
 
-//fun geraEstoqueGeral(outPath: String, estoque : MutableMap<String, Int>){
-//    val file = File(outPath + "/estoque_geral.csv")
-//
-//    file.printWriter().use {
-//
-//    }
-//
-//}
+fun leVendas(inPath: String, estoque_mp: MutableMap<String, InfoEstoque>, balancete_dt: InfoBalancete) {
+    val vendasFilePath = File(inPath + "/vendas.csv")
 
-fun main() {
-    var produtos_mp = mutableMapOf<String, Produto>()
-    var estoque_mp = mutableMapOf<String, Int>()
+    vendasFilePath.bufferedReader().useLines { lines ->
+        lines.drop(1).forEach { line ->
+            val values = line.split(',')
 
-    var comprasFilePath = "/home-temp/aluno/Downloads/Trab1-Mobile/Testes/exemplo_1/entrada/compras.csv"
+            val cod = values[0]
+            val qtd = values[1].toInt()
 
-    leEntradas(comprasFilePath, produtos_mp, estoque_mp)
+            if (estoque_mp.containsKey(cod)) {
+                estoque_mp[cod]!!.qtd -= qtd
 
-//    geraEstoqueGeral()
-
-
-    for(valor in produtos_mp.values){
-        println(valor)
+                balancete_dt.vendas += qtd * estoque_mp[cod]!!.prod.preco_venda
+                balancete_dt.balancete += qtd * estoque_mp[cod]!!.prod.preco_venda
+            } else {
+                println("Produto não presente no estoque")
+            }
+        }
     }
-    for(par in estoque_mp){
-        println(par)
+}
+
+fun geraEstoqueGeral(outPath: String, estoque: MutableMap<String, InfoEstoque>) {
+    val file = File(outPath + "/estoque_geral.csv")
+    file.createNewFile()
+
+    file.printWriter().use { writer ->
+        writer.println("CODIGO,NOME,QUANTIDADE")
+
+        for (value in estoque.values) {
+            writer.println("${value.prod.cod},${value.prod.nome},${value.qtd}")
+        }
+    }
+}
+
+fun geraEstoqueCategorias(outPath: String, estoque: MutableMap<String, InfoEstoque>) {
+    val file = File(outPath + "/estoque_categorias.csv")
+
+    var qtdRoupa = 0
+    var qtdColecionavel = 0
+    var qtdEletronico = 0
+
+    for (value in estoque.values) {
+        if (value.prod is Roupa)
+            qtdRoupa += value.qtd
+        else if (value.prod is Colecionavel)
+            qtdColecionavel += value.qtd
+        else if (value.prod is Eletronico)
+            qtdEletronico += value.qtd
     }
 
+    file.createNewFile()
+    file.printWriter().use { writer ->
+        writer.println("CATEGORIA,QUANTIDADE")
+        writer.println("ROUPA,${qtdRoupa}")
+        writer.println("COLECIONAVEL,${qtdColecionavel}")
+        writer.println("ELETRONICO,${qtdEletronico}")
+    }
+}
+
+fun geraBalancete(outPath: String, balancete_dt: InfoBalancete) {
+    val balanceteFilePath = File(outPath + "/balancete.csv")
+
+    balanceteFilePath.createNewFile()
+    balanceteFilePath.printWriter().use { writer ->
+        writer.println("COMPRAS,${balancete_dt.compras}")
+        writer.println("VENDAS,${balancete_dt.vendas}")
+        writer.println("BALANCETE,${balancete_dt.balancete}")
+    }
+}
+
+data class InfoBusca(
+    val categoria: String,
+    val tipo: String,
+    val tamanho: String,
+    val cor_primaria: String,
+    val cor_secundaria: String,
+    val versao: String,
+    val ano: String,
+    val material: String,
+    val relevancia: String
+)
+
+fun buscaQuantidadeNoEstoque(info_dt: InfoBusca, estoque_mp: MutableMap<String, InfoEstoque>): Int {
+
+    return estoque_mp.values.filter{ it.prod.atendeAosCriterios(info_dt) }.sumOf { it.qtd }
+}
+
+fun realizaBusca(inPath: String, outPath: String, estoque_mp: MutableMap<String, InfoEstoque>) {
+    var qtdBuscas = mutableListOf<Int>()
+
+    val buscasFile = File(inPath + "/busca.csv")
+
+    buscasFile.bufferedReader().useLines { lines ->
+        var lineCount = 0
+
+        lines.drop(1).forEach { line ->
+            var values = line.split(',')
+            values = values.map { it.uppercase() }
+
+            val info_dt = InfoBusca(
+                values[0],
+                values[1],
+                values[2],
+                values[3],
+                values[4],
+                values[5],
+                values[6],
+                values[7],
+                values[8]
+            )
+
+            val qtd = buscaQuantidadeNoEstoque(info_dt, estoque_mp)
+            qtdBuscas.add(qtd)
+        }
+    }
+
+    val resultadoFile = File(outPath + "/resultado_buscas.csv")
+    resultadoFile.createNewFile()
+
+    resultadoFile.printWriter().use { writer ->
+        writer.println("BUSCAS,QUANTIDADE")
+
+        for((idx, qtd) in qtdBuscas.withIndex()){
+            writer.println("${idx + 1},${qtd}")
+        }
+    }
+
+}
+
+fun main(args: Array<String>) {
+    var estoque_mp = mutableMapOf<String, InfoEstoque>()
+    var balancete_dt = InfoBalancete(0.0, 0.0, 0.0)
+//    var inPath = args[0]
+//    var outPath = args[1]
+
+    var inPath = "/home/leticia/Desktop/Trab-Mobile/Testes/exemplo_1/entrada"
+    var outPath = "/home/leticia/Desktop/Trab-Mobile/Testes/exemplo_1/minha_saida"
+
+    leCompras(inPath, estoque_mp, balancete_dt)
+    leVendas(inPath, estoque_mp, balancete_dt)
+
+    for(value in estoque_mp.values){
+        println(value)
+    }
+
+    geraEstoqueGeral(outPath, estoque_mp)
+    geraEstoqueCategorias(outPath, estoque_mp)
+    geraBalancete(outPath, balancete_dt)
+
+    realizaBusca(inPath, outPath, estoque_mp)
 }
